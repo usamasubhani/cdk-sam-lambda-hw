2162fcf9c4df57ac6548fcb58b0b2f3105c9aa75
Usama Subhani <reachusama@protonmail.com>
2020-11-26 21:18:21 +0500

    Add API Gateway to stack

--- lib/cdk-sam-lambda-stack.ts
+++ lib/cdk-sam-lambda-stack.ts
@@ -1,14 +1,17 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway'
 
 export class CdkSamLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const hello = new lambda.Function(this, 'MyFunction' , {
+    const myLambdaFunc = new lambda.Function(this, 'MyFunction' , {
       runtime: lambda.Runtime.PYTHON_3_7,
       handler: 'app.lambda_handler',
       code: lambda.Code.fromAsset('./my_function')
     })
+
+    new apigateway.LambdaRestApi(this, "API", {handler: myLambdaFunc})
   }
 }
