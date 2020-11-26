6d55cde798f7fa7bde2e89fc0af4d76ef7759e45
Usama Subhani <reachusama@protonmail.com>
2020-11-26 15:58:36 +0500

    Add lambda function MyFunction to stack

--- lib/cdk-sam-lambda-stack.ts
+++ lib/cdk-sam-lambda-stack.ts
@@ -1,9 +1,14 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class CdkSamLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const hello = new lambda.Function(this, 'MyFunction' , {
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: 'app.lambda_handler',
+      code: lambda.Code.fromAsset('./my_function')
+    })
   }
 }
